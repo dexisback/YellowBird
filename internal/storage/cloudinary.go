c41d66462c216cc2b9d006efdaf40f1d0457c6df
Amaan <texamaan86@gmail.com>
2026-08-03 13:46:11 +0530

    add(storage layer): instantiate cloudinary (future s3 support) | config/interface/cloudinary sdk

---
+++ internal/storage/cloudinary.go
@@ -0,0 +1,73 @@
+package storage
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api/uploader"
+)
+
+type CloudinaryStorage struct {
+	client *cloudinary.Cloudinary
+}
+
+func NewCloudinaryStorage(cloudName, apiKey, apiSecret string) (*CloudinaryStorage, error) {
+	cld, err := cloudinary.NewFromParams(cloudName, apiKey, apiSecret)
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialise cloudinary: %w", err)
+	}
+	return &CloudinaryStorage{
+		client: cld,
+	}, nil
+}
+
+func (s *CloudinaryStorage) Upload(
+	ctx context.Context,
+	input UploadInput,
+) (*UploadResult, error) {
+	result, err := s.client.Upload.Upload(
+		ctx,
+		input.File,
+		uploader.UploadParams{
+			PublicID: input.FileName,
+			Folder:   "YellowBird",
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	originalFileName := input.FileName
+	if input.Header != nil && input.Header.Filename != "" {
+		originalFileName = input.Header.Filename
+	}
+
+	mimeType := ""
+	if input.Header != nil {
+		mimeType = input.Header.Header.Get("Content-Type")
+	}
+
+	var size int64
+	if input.Header != nil {
+		size = input.Header.Size
+	} else {
+		size = int64(result.Bytes)
+	}
+
+	return &UploadResult{
+		StorageKey:       result.PublicID,
+		URL:              result.SecureURL,
+		OriginalFileName: originalFileName,
+		MimeType:         mimeType,
+		Size:             size,
+	}, nil
+}
+
+func (s *CloudinaryStorage) Delete(
+	ctx context.Context,
+	storageKey string,
+) error {
+	_, err := s.Client.Upload > Destroy(
+		ctx,
+	)
+}
---
+++ internal/storage/interface.go
@@ -0,0 +1,30 @@
+package storage
+
+//what this does -> defines the contract that every storage provider (cloudinary/s3/the stealth wala im gonna be using/local disk) must implement
+// //scalable
+//
+//
+import (
+	"context"
+	"mime/multipart"
+)
+
+type UploadInput struct {
+	File     multipart.File
+	Header   *multipart.FileHeader
+	FileName string
+}
+
+type UploadResult struct {
+	StorageKey       string
+	URL              string
+	OriginalFileName string
+
+	MimeType string
+	Size     int64
+}
+
+type Storage interface {
+	Upload(ctx context.Context, input UploadInput) (*UploadResult, error)
+	Delete(ctx context.Context, storageKey string) error
+}
